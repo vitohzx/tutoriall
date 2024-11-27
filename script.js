d4ec2aa2e5276828e690d27bbdb0cfec5a997d29
Vitor <163857646+vitohzx@users.noreply.github.com>
2024-11-27 10:39:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//estrutura de repetição para somar todos os numeros do 1 ao 500
+
+let incremento = 1
+let somador = 0
+
+do {
+    somador += incremento
+    incremento++
+} while (incremento <= 500)
+
+console.log(somador)
+
